821794f43201715d9197a11628e152a4c1b7a9f9
KYokoyama <8509057+KaiseiYokoyama@users.noreply.github.com>
2021-03-20 20:48:28 +0900

    add Distribution<C>
    normalized Frequencies<C>

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod tests {
 }
 
 pub struct Frequencies<C: std::hash::Hash + std::cmp::Eq> {
-    // map of frequencies
+    /// map of frequencies
     map: HashMap<C, u128>,
 }
 
@@ -23,3 +23,24 @@ impl<C: std::hash::Hash + std::cmp::Eq> Frequencies<C> {
         }
     }
 }
+
+pub struct Distribution<'a, C: std::hash::Hash + std::cmp::Eq> {
+    /// map of distribution
+    map: HashMap<&'a C, f64>,
+}
+
+impl<'a, C: std::hash::Hash + std::cmp::Eq> Distribution<'a, C> {
+    pub fn new(frequencies: &'a Frequencies<C>) -> Self {
+        let n = frequencies.map.values()
+            .sum::<u128>() as f64;
+
+        let map = frequencies.map.iter()
+            .map(|(k,&v)| {
+                (k, v as f64 / n)
+            })
+            .collect();
+
+        Self { map }
+    }
+}
+
