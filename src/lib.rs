177e6d6b0a9ecfd363a866b4710b31e7f0f45240
KYokoyama <8509057+KaiseiYokoyama@users.noreply.github.com>
2021-03-20 20:29:57 +0900

    add Frequencies<C>
    C means character, but you can make C correspond to any other type that implements Hash and Eq.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
+use std::collections::HashMap;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+}
+
+pub struct Frequencies<C: std::hash::Hash + std::cmp::Eq> {
+    // map of frequencies
+    map: HashMap<C, u128>,
+}
+
+impl<C: std::hash::Hash + std::cmp::Eq> Frequencies<C> {
+    /// record an appearance of C
+    pub fn record(&mut self, c: C) {
+        if let Some(record) = self.map.get_mut(&c) {
+            *record += 1;
+        } else {
+            self.map.insert(c, 1);
+        }
     }
 }
