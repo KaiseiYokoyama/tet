d3ebbab53435ff93512b5974a7c101dca6408ccd
KYokoyama <8509057+KaiseiYokoyama@users.noreply.github.com>
2021-03-22 15:45:55 +0900

    add documentations

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,71 @@
-use crate::distribution::{Distribution, Frequencies};
+//! # Text Entry Throughput
+//! Text entry throughput [introduced by Minguri et al.](https://dl.acm.org/doi/fullHtml/10.1145/3290605.3300866)
+//! is a text entry method-independent throughput metric based on Shannon information theory.
+//!
+//! This crate is a third-party implementation of TET.
+//!
+//! ## TL;DR (English alphabet only)
+//! ```
+//! use tet::TextEntryThroughput;
+//!
+//! let tet = TextEntryThroughput::alphabet_letter_distribution();
+//!
+//! let presented_text = "my watch fell in the waterprevailing wind from the east";
+//! let transcribed_text = "my wacch fell in waterpreviling wind on the east";
+//! let s = std::time::Duration::from_secs(12); // 4 characters per second
+//!
+//! let throughput = tet.calc(presented_text, transcribed_text, s).unwrap();
+//! assert!((throughput - 12.954965333409255).abs() < 0.0001);
+//! ```
+//!
+//! ## Usage
+//! ### Get distribution
+//! First, prepare a distribution of characters to get entropy `H(X)` from source.
+//! ```
+//! use tet::{Frequencies, Distribution};
+//!
+//! let mut frequency = Frequencies::new();
+//!
+//! // get frequency of each character
+//! let source = "large and appropriate text is recommended";
+//! source.chars()
+//!     .for_each(|c| {
+//!         frequency.record(c.clone());
+//!     });
+//!
+//! // normalize frequency to get distribution
+//! let distribution = Distribution::new(frequency);
+//! ```
+//!
+//! ### Compute TET
+//! ```
+//! # use tet::*;
+//! # use tet::{Frequencies, Distribution};
+//! #
+//! # let mut frequency = Frequencies::new();
+//! #
+//! # // get frequency of each character
+//! # let source = "large and appropriate text is recommended";
+//! # source.chars()
+//! #     .for_each(|c| {
+//! #         frequency.record(c.clone());
+//! #     });
+//! #
+//! # // normalize frequency to get distribution
+//! # let distribution = Distribution::new(frequency);
+//! #
+//! // now you can calculate TET! :+1:
+//! let tet = TextEntryThroughput::new(distribution);
+//!
+//! let (presented, transcribed) = ("quickly", "qucehkly");
+//! let s = std::time::Duration::from_secs(2); // 4 characters per minute
+//!
+//! // Text Entry Throughput (bits/second)
+//! let throughput = tet.calc(presented, transcribed, s).unwrap();
+//! ```
+
+
+pub use crate::distribution::{Distribution, Frequencies};
 use std::collections::HashMap;
 
 mod distribution;
@@ -80,6 +147,6 @@ mod test {
         // -> significant digits
         // 3.238 * 4.000 = 12.952 (on the paper)
         // paper: https://dl.acm.org/doi/fullHtml/10.1145/3290605.3300866
-        assert_eq!(throughput, 12.954965333409255);
+        assert!((throughput - 12.954965333409255).abs() < 0.0001);
     }
 }
